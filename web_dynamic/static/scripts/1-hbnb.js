1364e0f1d1c091e879e120c3a50ca82fac21d825
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-28 22:48:57 +0200

    Handled amenities list overflow over amenities div width

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,19 +1,23 @@
 // Your script must be executed only when DOM is loaded
-$( document ).ready(() => {
-// Listen for changes on each input checkbox tag:
-$( 'input[type=checkbox]' ).change(() => {
-	amenityIdList = [];
-	// if the checkbox is checked, you must store the Amenity ID in a variable (dictionary or list)
-	if (this.checked) {
-		amenityIdList.push(this.data_id);
-	} else if (!this.checked) {
-	// if the checkbox is unchecked, you must remove the Amenity ID from the variable
-		amenityIdList.remove(this.data_id);
-	}
-	// update the h4 tag inside the div Amenities with the list of Amenities checked
-	for (amenityId of amenityIdList) {
-		$( 'div:amenities, h4' )
-	}
-	});
-});
+$(document).ready(function() {
+	const maxLength = 30;
+	const amenityIdDict = {};
+	// Listen for changes on each input checkbox tag:
+	$('input[type=checkbox]').change(function() {
 
+		// if the checkbox is checked, you must store the Amenity ID in a variable (dictionary or list)
+		if (this.checked) {
+			amenityIdDict[$(this).data('id')]=$(this).data('name');
+		} else {
+		// if the checkbox is unchecked, you must remove the Amenity ID from the variable
+			delete amenityIdDict[$(this).data('id')];
+		}
+
+		// update the h4 tag inside the div Amenities with the list of Amenities checked
+		const amenityNames = Object.values(amenityIdDict).join(', ');
+		const truncatedText = amenityNames.length > maxLength
+      ? amenityNames.substring(0, maxLength) + '...'
+      : amenityNames;
+		$('.amenities').find('h4').text(truncatedText);
+		});
+});
