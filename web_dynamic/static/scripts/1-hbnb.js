ed687de666c443ac48222eabb29e403eddd13978
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-29 15:43:09 +0200

    Fixed and completed comments

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,22 +1,27 @@
-// Your script must be executed only when DOM is loaded
+// Script execute only when DOM is loaded
 $(document).ready(function () {
-  const maxLength = 30;
-  const amenityIdDict = {};
-  // Listen for changes on each input checkbox tag:
-  $('input[type=checkbox]').change(function () {
-    // if the checkbox is checked, you must store the Amenity ID in a variable (dictionary or list)
-    if (this.checked) {
-      amenityIdDict[$(this).data('id')] = $(this).data('name');
-    } else {
-      // if the checkbox is unchecked, you must remove the Amenity ID from the variable
-      delete amenityIdDict[$(this).data('id')];
-    }
+	// Define the maximum length for the displayed amenity names
+	const maxLength = 30;
 
-    // update the h4 tag inside the div Amenities with the list of Amenities checked
-    const amenityNames = Object.values(amenityIdDict).join(', ');
-    const truncatedText = amenityNames.length > maxLength
-      ? amenityNames.substring(0, maxLength) + '...'
-      : amenityNames;
-    $('.amenities').find('h4').text(truncatedText);
-  });
+	// Dictionary to store the selected amenity IDs and names
+	const amenityIdDict = {};
+
+	// Listen for changes on each input checkbox tag:
+	$('input[type=checkbox]').change(function () {
+		// If the checkbox is checked, store the Amenity ID in the dictionary
+		if (this.checked) {
+			amenityIdDict[$(this).data('id')] = $(this).data('name');
+		} else {
+			// If the checkbox is unchecked, remove the Amenity ID from the dictionary
+			delete amenityIdDict[$(this).data('id')];
+		}
+
+		// Update the h4 tag inside the div Amenities with the list of checked amenity names
+		const amenityNames = Object.values(amenityIdDict).join(', ');
+		const truncatedText =
+			amenityNames.length > maxLength
+				? amenityNames.substring(0, maxLength) + '...'
+				: amenityNames;
+		$('.amenities').find('h4').text(truncatedText);
+	});
 });
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,38 +1,49 @@
-// Your script must be executed only when DOM is loaded
-$(document).ready(function () {});
-const maxLength = 30;
-const amenityIdDict = {};
-// Listen for changes on each input checkbox tag:
-$('input[type=checkbox]').change(function () {
-  // if the checkbox is checked, you must store the Amenity ID in a variable (dictionary or list)
-  if (this.checked) {
-    amenityIdDict[$(this).data('id')] = $(this).data('name');
-  } else {
-    // if the checkbox is unchecked, you must remove the Amenity ID from the variable
-    delete amenityIdDict[$(this).data('id')];
-  }
+// Script execute only when DOM is loaded
+$(document).ready(function () {
+	// Define the maximum length for the displayed amenity names
+	const maxLength = 30;
 
-  // update the h4 tag inside the div Amenities with the list of Amenities checked
-  const amenityNames = Object.values(amenityIdDict).join(', ');
-  const truncatedText =
-    amenityNames.length > maxLength
-      ? amenityNames.substring(0, maxLength) + '...'
-      : amenityNames;
-  $('.amenities').find('h4').text(truncatedText);
+	// Dictionary to store the selected amenity IDs and names
+	const amenityIdDict = {};
+
+	// Listen for changes on each input checkbox tag:
+	$('input[type=checkbox]').change(function () {
+		// If the checkbox is checked, store the Amenity ID in the dictionary
+		if (this.checked) {
+			amenityIdDict[$(this).data('id')] = $(this).data('name');
+		} else {
+			// If the checkbox is unchecked, remove the Amenity ID from the dictionary
+			delete amenityIdDict[$(this).data('id')];
+		}
+
+		// Update the h4 tag inside the div Amenities with the list of checked amenity names
+		const amenityNames = Object.values(amenityIdDict).join(', ');
+		const truncatedText =
+			amenityNames.length > maxLength
+				? amenityNames.substring(0, maxLength) + '...'
+				: amenityNames;
+		$('.amenities').find('h4').text(truncatedText);
+	});
 });
 
-function checkApiStatus () {
-  $.get('http://localhost:5001/api/v1/status/')
-    .done(function (response) {
-      if (response.status === 'OK') {
-        $('#api_status').addClass('available');
-      } else {
-        $('#api_status').removeClass('available');
-      }
-    })
-    .fail(function () {
-      $('#api_status').removeClass('available');
-    });
+// Function to check the status of the API
+function checkApiStatus() {
+	// Send a GET request to the API status endpoint
+	$.get('http://localhost:5001/api/v1/status/')
+		.done(function (response) {
+			// If the response status is 'OK', add the 'available' class to the element with id 'api_status'
+			if (response.status === 'OK') {
+				$('#api_status').addClass('available');
+			} else {
+				// If the response status is not 'OK', remove the 'available' class from the element with id 'api_status'
+				$('#api_status').removeClass('available');
+			}
+		})
+		.fail(function () {
+			// If the request fails, remove the 'available' class from the element with id 'api_status'
+			$('#api_status').removeClass('available');
+		});
 }
 
+// Call the checkApiStatus function to check the API status
 checkApiStatus();
