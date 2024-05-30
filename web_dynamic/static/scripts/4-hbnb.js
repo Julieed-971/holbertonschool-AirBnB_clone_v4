0b251ca1ce4e0c6bb45a3fc479e00831e2b4e529
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-30 16:54:08 +0200

    Filter places by Amenity

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,35 +1,3 @@
-// Script execute only when DOM is loaded
-$(document).ready(function () {
-  // Define the maximum length for the displayed amenity names
-  const maxLength = 30;
-
-  // Dictionary to store the selected amenity IDs and names
-  const amenityIdDict = {};
-
-  // Listen for changes on each input checkbox tag:
-  $('input[type=checkbox]').change(function () {
-    // If the checkbox is checked, store the Amenity ID in the dictionary
-    if (this.checked) {
-      amenityIdDict[$(this).data('id')] = $(this).data('name');
-    } else {
-      // If the checkbox is unchecked, remove the Amenity ID from the dictionary
-      delete amenityIdDict[$(this).data('id')];
-    }
-
-    // Update the h4 tag inside the div Amenities with the list of checked amenity names
-    const amenityNames = Object.values(amenityIdDict).join(', ');
-    const truncatedText =
-      amenityNames.length > maxLength
-        ? amenityNames.substring(0, maxLength) + '...'
-        : amenityNames;
-    $('.amenities').find('h4').text(truncatedText);
-  });
-
-  // Call the checkApiStatus function to check the API status
-  checkApiStatus();
-  fetchPlaces();
-});
-
 // Function to check the status of the API
 function checkApiStatus () {
   // Send a GET request to the API status endpoint
@@ -49,15 +17,16 @@ function checkApiStatus () {
     });
 }
 
-function fetchPlaces () {
+function fetchPlaces (data = {}) {
   // Send a POST request with Content-Type: application/json and an empty dictionary in the body - cURL version:
   $.ajax({
     url: 'http://localhost:5001/api/v1/places_search/',
     type: 'POST',
-    data: JSON.stringify({}),
+    data: JSON.stringify(data),
     contentType: 'application/json',
     success: function (response) {
-      // Loop into the result of the request and
+      // Loop into the result of the request
+      // Handle if search button is clicked
       $.each(response, function (index, place) {
         // Create a new article element for each place and add place data to the article section
         const article = $('<article></article>');
@@ -90,3 +59,45 @@ function fetchPlaces () {
     }
   });
 }
+
+// Script execute only when DOM is loaded
+$(document).ready(function () {
+  // Define the maximum length for the displayed amenity names
+  const maxLength = 30;
+
+  // Dictionary to store the selected amenity IDs and names
+  const amenityIdDict = {};
+
+  // Listen for changes on each input checkbox tag:
+  $('input[type=checkbox]').change(function () {
+    // If the checkbox is checked, store the Amenity ID in the dictionary
+    if (this.checked) {
+      amenityIdDict[$(this).data('id')] = $(this).data('name');
+    } else {
+      // If the checkbox is unchecked, remove the Amenity ID from the dictionary
+      delete amenityIdDict[$(this).data('id')];
+    }
+
+    // Update the h4 tag inside the div Amenities with the list of checked amenity names
+    const amenityNames = Object.values(amenityIdDict).join(', ');
+    const truncatedText =
+      amenityNames.length > maxLength
+        ? amenityNames.substring(0, maxLength) + '...'
+        : amenityNames;
+    $('.amenities').find('h4').text(truncatedText);
+  });
+
+  // Call the checkApiStatus function to check the API status
+  checkApiStatus();
+  // Fetch all places
+  fetchPlaces();
+  // When the button tag is clicked,
+  $('button').click(function () {
+    $('.places').empty();
+    // Fetch and display all places filtered by amenities
+    const idsList = Object.keys(amenityIdDict);
+    fetchPlaces({
+      amenities: idsList
+    });
+  });
+});
